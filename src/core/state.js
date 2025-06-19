3644dedc0908ea26fff4d3bb4fcb714a284e8e76
Vandern Rodrigues <nrednav@gmail.com>
2025-06-19 17:37:38 +0100

    define initial app state

---
+++ src/core/state.js
@@ -0,0 +1,19 @@
+/**
+ * The initial, empty state of the application.
+ *
+ * @typedef {object} Bookmark
+ * @property {string} id - A unique identifier for the bookmark
+ * @property {string} content - The text or HTML content of the bookmarked response.
+ * @property {string[]} tags - An array of user-defined tags.
+ * @property {number} timestamp - The UTC timestamp when the bookmark was created.
+ *
+ * @typedef {object} AppState
+ * @property {Bookmark[]} bookmarks - An array of all bookmark objects for the current conversation.
+ */
+
+/**
+ * @type {AppState}
+ */
+export const initialState = {
+  bookmarks: []
+};
