b9f337426c74f54fb09af586cf8e7e2f34b89520
Vandern Rodrigues <nrednav@gmail.com>
2025-06-21 14:32:34 +0100

    setup build system

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig, loadEnv } from "vite";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+
+function generateManifest() {
+  const manifest = readJsonFile("src/manifest.json");
+  const pkg = readJsonFile("package.json");
+
+  return { version: pkg.version.split("-")[0], ...manifest };
+}
+
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, ".");
+
+  return {
+    plugins: [
+      webExtension({
+        manifest: generateManifest,
+        watchFilePaths: ["package.json", "src/manifest.json"],
+        browser: env.VITE_TARGET_BROWSER || "chrome",
+        webExtConfig: {}
+      })
+    ],
+    build: {
+      sourcemap: "inline",
+      outDir: "dist",
+      emptyOutDir: true
+    },
+    resolve: {
+      alias: {
+        src: "/src"
+      }
+    }
+  };
+});
