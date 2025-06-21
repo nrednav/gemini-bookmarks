a87a8ec7a70b562f3d8165c9761ad18742a23764
Vandern Rodrigues <nrednav@gmail.com>
2025-06-21 19:03:19 +0100

    install jsdom and add test suite for wait-for-element helper

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,9 @@ export default defineConfig((config) => {
       outDir: "dist",
       emptyOutDir: true
     },
+    test: {
+      environment: 'jsdom'
+    },
     resolve: {
       alias: {
         src: "/src"
